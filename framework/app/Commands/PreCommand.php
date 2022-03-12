951deed623be92408dfa3f4c9a5757e83261f262
denn <shuvalov.1243@gmail.com>
2022-03-12 11:40:58 +0300

    singleton script

--- framework/app/Commands/PreCommand.php
+++ framework/app/Commands/PreCommand.php
@@ -4,12 +4,25 @@ namespace App\Commands;
 
 class PreCommand
 {
-    public $slug = 'pre';
 
     public function run()
     {
+        $lockFile = __FILE__ . '.lock';
+        $hasFile = file_exists($lockFile);
+        $lockFp = fopen($lockFile, 'w');
+        if (!flock($lockFp, LOCK_EX | LOCK_NB)) {
+            die('Экзепляр уже запущен');
+        }
+        if ($hasFile) {
+            echo 'Предыдущий запуск завершился с ошибкой';
+        }
+
         echo "<pre>" . print_r('anime top', true) . "</pre>";
-        die;
+
+        register_shutdown_function(function () use ($lockFp, $lockFile) {
+            flock($lockFp, LOCK_UN);
+            unlink($lockFile);
+        });
     }
 
 }
